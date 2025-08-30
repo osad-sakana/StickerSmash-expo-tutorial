14cc863a7bd49f6d9da47587f18bfc6f32f79669
osad-sakana <mezzoforte.mackerel@icloud.com>
2025-08-30 23:06:08 +0900

    aboutページとルーティングを追加

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,8 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>
+    <Stack.Screen name="index" options={{ title: "ホーム" }} />
+    <Stack.Screen name="about" options={{ title: "〜とは" }} />
+  </Stack>;
 }
--- app/about.tsx
+++ app/about.tsx
@@ -1,11 +1,11 @@
 import { Text, View, StyleSheet } from "react-native";
-import { Link } from 'expo-router';
+import { Link } from "expo-router";
 
-export default function Index() {
+export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>ホームスクリーン</Text>
-      <Link href="/about" style={styles.button}>〜とは</Link>
+      <Text style={styles.text}>「〜とは」スクリーン</Text>
+      <Link href="/" style={styles.button}>ホームに戻る</Link>
     </View>
   );
 }
